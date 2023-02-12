7d1fb00d8f0330809c7b1965eec938a6db1b67f1
KANAK RAJ <72430136+kanak-raj@users.noreply.github.com>
2023-02-12 17:42:07 +0530

    Add files via upload

---
+++ Eureka_demo/src/test/java/com/Eureka_demo/Eureka_demo/EurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eureka_demo.Eureka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
